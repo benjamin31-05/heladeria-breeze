69e4b8b07d41b464e97417818f9d6f4de0c178ab
Carloszzz10 <carlosedullacsahuangaabad3@gmail.com>
2025-04-14 01:48:15 -0500

    Commit inicial, configuración inicial del proyecto

---
+++ src/main/java/com/utp/herramientasDedesarrollo/HerramientasDedesarrolloApplication.java
@@ -0,0 +1,13 @@
+package com.utp.herramientasDedesarrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerramientasDedesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerramientasDedesarrolloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utp/herramientasDedesarrollo/HerramientasDedesarrolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.herramientasDedesarrollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerramientasDedesarrolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
